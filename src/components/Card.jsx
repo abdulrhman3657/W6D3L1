6323c24f40afad79267ba8cd4c8fd48c26f55e12
user1 <user01@em/com>
2025-05-20 13:28:31 +0300

    c4

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -49,7 +49,7 @@ function Card() {
         <div className="lg:grid grid-cols-3 gap-5 p-5">
           {cards.map((item) => {
             return (
-              <div className="flex flex-wrap mt-5 border p-3">
+              <div className="flex align-center justify-center flex-wrap mt-5 border p-3">
                 <img className=" w-60 h-60" src={item.img} alt="" />
                 <div className="p-4">
                   <h1 className="text-4xl mb-4">{item.name}</h1>
@@ -61,22 +61,31 @@ function Card() {
         </div>
       </div>
 
-
-          <div className="flex flex-wrap p-3 m-5 items-center justify-around border">
-            <div>
-              <h1 className="font-bold py-5">Bose Bluetooth woreless headphones</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-                tempora totam ipsam hic non! Ut, unde.
-              </p>
-            </div>
-            <img
-              className="lg:w-100 w-50 py-9"
-              src="https://m.media-amazon.com/images/I/81hbmwvrDiL._AC_SL1500_.jpg"
-            />
+      <div className="">
+        <div className="flex flex-wrap p-3 m-5 items-center justify-around border bg-amber-50">
+          <div>
+            <h1 className="font-bold py-5">
+              Bose Bluetooth woreless headphones
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+              tempora totam ipsam hic non! Ut, unde.
+            </p>
           </div>
+          <div>
+            <p className="text-4xl">300$</p>
+            <p className="text-green-600 text-4xl">150$</p>
+            <button className="bg-yellow-500 h-10 w-30 mt-3 rounded">
+              buy know
+            </button>
+          </div>
+          <img
+            className="lg:w-100 w-50 py-9"
+            src="https://m.media-amazon.com/images/I/31WcMiritWL._AC_.jpg"
+          />
         </div>
-
+      </div>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { CiMenuBurger } from "react-icons/ci";
 
 function Navbar() {
   return (
-    <div>
+    <div className=''>
         {/* <nav className='flex justify-between p-6 bg-gray-700  text-white'>
             <img className='rounded' width={"40px"} height={"40px"} src="https://static.vecteezy.com/system/resources/thumbnails/008/826/851/small/abstract-initial-letter-mz-logo-in-black-color-isolated-in-white-background-applied-for-clothing-brand-logo-also-suitable-for-the-brands-or-companies-that-have-an-initial-name-zm-vector.jpg" alt="" />
             <ul className='md:flex gap-10 hidden  font-extrabold'>
@@ -14,7 +14,7 @@ function Navbar() {
             <button className='bg-green-500 lg:block hidden w-20 h-10 rounded-lg text-black'>login</button>
             <CiMenuBurger className='text-white lg: hidden' />
         </nav> */}
-        <nav className='bg-yellow-300'>
+        <nav className='bg-yellow-300 lg:block'>
           <ul className='flex flex-wrap justify-around p-5 font-bold'>
             <li>all categories</li>
             <li className='text-gray-400'>today deals</li>
